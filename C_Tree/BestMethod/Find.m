415d443005db1c7f1a725fb50623b6417dc04426
WJ-Lai <418320384@qq.com>
2018-04-11 13:35:38 +0800

    Signed-off-by: WJ-Lai <418320384@qq.com>找到最佳参数

--- C_Tree/BestMethod/Find.m
+++ C_Tree/BestMethod/Find.m
@@ -6,7 +6,7 @@ clear;clc;
 Distances = zeros(84,1);
 Linkage = zeros(84,1);
 %每个簇样本数量不低于12.5%
-SampleNumber = 0.125;
+SampleNumber = 0.3;
 %簇个数不大于3
 ClusterNumber = 3;
 
@@ -66,6 +66,83 @@ for k = 1:1:84
 end
 
 %按照cophenet到小，注意检验簇数量和簇个数是否满足
-Y = pdist(X,Distance_Method{11});
-Z = linkage(Y,Linkage_Method{3});
-H = dendrogram(Z,100);
+Min = ceil(SampleNumber*length(Data_Selected_Right));
+for k = 1:1:84
+    
+    Y = pdist(X,Distance_Method{Distances(k,1)});
+    Z = linkage(Y,Linkage_Method{Linkage(k,1)});
+    H = dendrogram(Z,100);
+    set(H,'Color','k');
+    title('聚类树形图（右转）','Fontsize',18);
+    xlabel('样本编号','Fontsize',14)
+    ylabel('标准化距离（average）','Fontsize',14)
+    
+    %由inconsistent得出最佳分类个数
+    %计算深度：100
+    W = inconsistent(Z,50);
+    %计算增幅最多的聚类序号
+    % d_inconsistent = zeros(max(size(W))-1,1);
+    for i = 1:(max(size(W))-1)
+        d_inconsistent(i,1) = W(i+1,4) - W(i,4);
+    end
+    
+    %自动判断最佳聚类个数,两类或三类
+    %若倒数第一大于倒数第二，则两类
+    if d_inconsistent(max(size(W))-1,1) > d_inconsistent(max(size(W))-2,1)
+        n = 2;
+        clu = cluster(Z,n);
+        cluA = 0;
+        cluB = 0;
+        for i = 1:length(clu)
+            if clu(i) == 1
+                clu_n(i,1) = {'Cluster1'};
+                cluA = cluA+1;
+            elseif clu(i) == 2
+                clu_n(i,1) = {'Cluster2'};
+                cluB = cluB+1;
+            end
+        end  
+        %若三类样本数均大于SampleNumber，则break
+        if cluA >= Min & cluB >= Min
+            cluA
+            cluB
+            break;
+        end
+
+        
+    else
+        n = 3;
+        clu = cluster(Z,n);
+        cluA = 0;
+        cluB = 0;
+        cluC = 0;
+        for i = 1:length(clu)
+            if clu(i) == 1
+                clu_n(i,1) = {'Cluster1'};
+                cluA = cluA+1;
+            elseif clu(i) == 2
+                clu_n(i,1) = {'Cluster2'};
+                cluB = cluB+1;
+            elseif clu(i) == 3
+                clu_n(i,1) = {'Cluster3'};
+                cluC = cluC+1;
+            end
+        end
+        
+        %若三类样本数均大于SampleNumber，则break
+        if cluA >= Min & cluB >= Min & cluC >= Min
+            cluA 
+            cluB 
+            cluC
+            break;
+        end
+        
+    end
+  
+end
+
+%找到最佳参数及其对应的cophenet
+k
+Distance_Method{Distances(k,1)}
+Linkage_Method{Linkage(k,1)}
+cophenet(Z,Y)
