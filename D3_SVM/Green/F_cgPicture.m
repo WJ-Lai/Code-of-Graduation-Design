aba6e82b3649b76fc782607fe1b881d8b6e80e47
WJ-Lai <418320384@qq.com>
2018-04-11 18:52:21 +0800

    可单次等高线

--- D3_SVM/Green/F_cgPicture.m
+++ D3_SVM/Green/F_cgPicture.m
@@ -7,21 +7,21 @@ TexNnumber = 2;
 K = 5;
 
 g_ini = 0.01;
-g_finlVal = 2;
-g_goin = 0.08;
-g_bujin = g_finlVal/g_goin;
+g_finlVal = 0.51;
+g_goin = 0.05;
+g_bujin = (g_finlVal-g_ini)/g_goin +1;
 g = g_ini:g_goin:g_finlVal;
 
 c_ini = 0;
-c_finlVal = 0.5;
+c_finlVal = 5;
 c_goin = 0.5;
-c_bujin = c_finlVal/c_goin;
+c_bujin = (c_finlVal-c_ini)/c_goin +1;
 c = c_ini:c_goin:c_finlVal;
 
 TreeNumber = 30;
 
-New = zeros(TexNnumber,g_bujin);
-Tradition = zeros(TexNnumber,g_bujin);
+New = zeros(c_bujin,g_bujin);
+Tradition = zeros(c_bujin,g_bujin);
     
 NewAcc = zeros(c_bujin,g_bujin);
 OldAcc = zeros(c_bujin,g_bujin);
@@ -36,18 +36,25 @@ for i_c = 1:1:c_bujin
         
         %新方法        
         for i_g = 1:1:g_bujin
-            [AccuracySVM AccuracySVM_mean AccuracyRF AccuracyRF_mean] = NewKfold(K, Label, Data,c(),g,TreeNumber);
-            compare(1,1) = AccuracySVM_mean;
+            [AccuracySVM AccuracySVM_mean AccuracyRF AccuracyRF_mean] = NewKfold(K, Label, Data,c(1,i_c),g(1,i_g),TreeNumber);
+            NewAcc(i_c,i_g) = AccuracySVM_mean;
         end
         
         %传统方法
         for i_g = 1:1:g_bujin
-            Label = LabelRecovery(Label)
-            [AccuracySVM AccuracySVM_mean AccuracyRF AccuracyRF_mean] = Kfold(K, Label, Data,c,g,TreeNumber);
-            compare(2,1) = AccuracySVM_mean;
+            Label = LabelRecovery(Label);
+            [AccuracySVM AccuracySVM_mean AccuracyRF AccuracyRF_mean] = Kfold(K, Label, Data,c(1,i_c),g(1,i_g),TreeNumber);
+            OldAcc(i_c,i_g) = AccuracySVM_mean;
         end
         
- end
+end
+
+figure();
+[C,h] = contour(c,g,NewAcc);
+clabel(C,h);
+figure();
+[C,h] = contour(c,g,OldAcc)
+clabel(C,h);
 
 % 
 % figure();
